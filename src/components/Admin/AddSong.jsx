32864e918db1dede6813c956b66219ebbb407ff6
Timothy Babatunde Akanbi <97198113+TimothyBabatu13@users.noreply.github.com>
2024-01-03 16:44:49 +0100

    Add files via upload
    Worked on validating if inputs are empty or not

--- src/components/Admin/AddSong.jsx
+++ src/components/Admin/AddSong.jsx
@@ -1,15 +1,14 @@
-import { useState, useRef, useContext } from "react";
+import { useState, useContext } from "react";
 import Api from "../Api";
 import App from "../../App";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import RenderSongFIleInput from "./RenderSongFIleInput";
-import { Form } from "react-router-dom";
 import {getStorage, uploadBytesResumable, ref, getDownloadURL} from "firebase/storage";
 
 const AddSong = ()=>{
-    const context = App.createContextHook;
-    const Context = useContext(context);
+    // const context = App.createContextHook;
+    // const Context = useContext(context);
     const [display, setDisplay] = useState(false);
     const [listOfTrack, setListOfTrack] = useState(['a']);
     const [form, setForm] = useState({
@@ -21,6 +20,15 @@ const AddSong = ()=>{
         type: ""
     })
 
+    function checkIfNotEmpty(obj) {
+        for (let key in obj) {
+          if (obj[key] === '') {
+            return false;
+          }
+        }
+        return true;
+      }
+
     const handleType = (e)=>{
         
         setForm(prev=>(
@@ -30,14 +38,12 @@ const AddSong = ()=>{
             }
         ))
     }
-    // console.log(form)
 
     const addSong = ()=>{
         setDisplay(prev => !prev)
     }
 
     const handleSelectSongFile = (e) =>{
-        // console.log(e.target.files[0])
         setForm(prev => ({
             ...prev,
             songFile: [...prev.songFile, e.target.files[0]]
@@ -45,7 +51,6 @@ const AddSong = ()=>{
     }
   
     const handleSelectImage = (e)=>{
-        // console.log(e.target.files[0])
         setForm(prev => ({
             ...prev,
             imgURL: e.target.files[0]
@@ -95,125 +100,24 @@ const AddSong = ()=>{
         );
     }
 
-    const sendMusicFile = (getData)=>{
-        Api.uploadFile(form.songFile, (audioLink)=>{
-            // console.log(getData);
-            const newData = {
-                ...getData,
-                songFile: audioLink
-            }
-            const dataToSend = {
-                author: newData.artist,
-                title: newData.title,
-                timePosted: new Date().getDay(),
-                voteNum: 1000,
-                desc: newData.description,
-                link: newData.songFile,
-                img: newData.imgURL,
-                typeof: newData.type
-            }
-            //author: "Omah Lay",
-        //     title: "What Have We Done(WHWD)",
-        //     timePosted: "12:40",
-        //     voteNum: 1,
-        //     id: 0,
-        //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, architecto sequi tempora, voluptatum voluptas vel voluptates ex corporis culpa omnis nemo quaerat, assumenda modi sint molestiae! Veniam doloremque provident impedit cumque enim, temporibus, facilis voluptatibus accusantium totam ut quo quis mollitia nostrum soluta nihil libero ipsam odit similique unde incidunt consequatur! Libero earum expedita, eius laudantium animi inventore doloribus nobis praesentium corporis id nulla quidem ad omnis dolore ducimus. Ipsum ut dignissimos corrupti praesentium nesciunt temporibus voluptatum, fugiat laboriosam eveniet quia aliquid animi, fugit vel asperiores eum eos repellendus ea odit dolore voluptate quidem veniam, sequi ipsam. Magnam, ea ratione.",
-        //     tracks: [
-        //         {
-        //         name: "ggodly",
-        //         link: "",
-        //         id: 0
-        //     },
-        //     {
-        //         name: "Godly",
-        //         link: "",
-        //         id: 1
-        //     },
-        // ]}
-            Api.sendData(form.type, dataToSend);
-            toast.success("data sent succesfully")
-        })
-        
-    }
-    //work on handleSubmit later. I havent achieved what I want.
-    const handleSubmit =  (e)=>{
-        e.preventDefault();
-        // const formArray = Object.fromEntries(form);
-        // const formArray = Object.values(form);
-        // console.log(formArray);
-        // const formArrayLength = formArray.filter(item => item.trim() === "").length;
-
-        // if(formArrayLength) return
-        // console.log("Hi")
-
-        // console.log(form)
-        // console.log(ref.current.files[0])
-        // console.log(formArray)
-        Api.uploadFile(form.imgURL, (imgLink)=>{
-            toast.error("Uploading data to backend...Do not press anything")
-            // console.log(imgLink)
-            const dataToSend = {
-                artist: form.artist,
-                title: form.title,
-                songFile: "",
-                imgURL: imgLink,
-                description: form.description,
-                type: form.type
-            }
-            sendMusicFile(dataToSend)
-        })
-        //     console.log("sent...")
-        // })
-        // console.log(form)
-        // Api.sendData(Context.category, {
-        //     author: "Omah Lay",
-        //     title: "What Have We Done(WHWD)",
-        //     timePosted: "12:40",
-        //     voteNum: 1,
-        //     id: 0,
-        //     desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, architecto sequi tempora, voluptatum voluptas vel voluptates ex corporis culpa omnis nemo quaerat, assumenda modi sint molestiae! Veniam doloremque provident impedit cumque enim, temporibus, facilis voluptatibus accusantium totam ut quo quis mollitia nostrum soluta nihil libero ipsam odit similique unde incidunt consequatur! Libero earum expedita, eius laudantium animi inventore doloribus nobis praesentium corporis id nulla quidem ad omnis dolore ducimus. Ipsum ut dignissimos corrupti praesentium nesciunt temporibus voluptatum, fugiat laboriosam eveniet quia aliquid animi, fugit vel asperiores eum eos repellendus ea odit dolore voluptate quidem veniam, sequi ipsam. Magnam, ea ratione.",
-        //     tracks: [
-        //         {
-        //         name: "ggodly",
-        //         link: "",
-        //         id: 0
-        //     },
-        //     {
-        //         name: "Godly",
-        //         link: "",
-        //         id: 1
-        //     },
-        // ]})
-    }
     const addMoreSong = (e)=>{
         e.preventDefault();
         setListOfTrack(prev => [...prev, "a"])
     }
  
-    const upLoadAll = (e)=>{
+    const handleSubmit = (e)=>{
 
         e.preventDefault();
+        const validate = checkIfNotEmpty(form);
+        if(!validate){
+            toast.error("One of the fields is empty. Please fill it and submit again");
+            return;
+        }
         const u = [];
-        let img
+        let img;
         form.songFile.forEach(item =>{
             toast.error("Uploading data to backend...Do not press anything");
             upLoadMusic(item, (downloadURL)=>{
-                // u.push(downloadURL)
-                // upLoadImage(form.imgURL, (e)=> img = e)
-                // u.push(downloadURL)
-                // const dataToSend = {
-                //     author: form.artist,
-                //     title: form.title,
-                //     timePosted: new Date().getDay(),
-                //     voteNum: 1000,
-                //     desc: form.description,
-                //     link: u,
-                //     typeof: form.type,
-                //     img
-                // }
-                // console.log(dataToSend)
-                // Api.sendData(form.type, dataToSend)
-                // toast.success("Data sent succesfully")
                 u.push(downloadURL);
                 upLoadImage(form.imgURL, (e)=>{
                     img = e;
@@ -228,19 +132,16 @@ const AddSong = ()=>{
                         img
                         }
                         console.log(dataToSend)
-                        Api.sendData(form.type, dataToSend);
-                        toast.success("Data sent succesfully")
+                        const sendDataNow = ()=>{
+                            Api.sendData(form.type, dataToSend);
+                            toast.success("Data sent succesfully");
+                        }
+                        return sendDataNow();
                 })
             })
         })
     }
-    const validate = ()=>{
-        for(let i in form){
-            if(form[i] === "") return true;
-        }
-        return false
-    }
-    // validate()
+    
     return(
         <div>
             <ToastContainer position="top-right" />
@@ -310,7 +211,7 @@ const AddSong = ()=>{
                     <button style={{"display":"block", "marginRight":"20px" ,"marginTop":"10px", "padding":"10px 15px", "cursor":"pointer", "background":"blue", "border":"none", "borderRadius":"5px"}} onClick={handleSubmit} type="submit">Submit</button>
                     <button style={{"display":"block", "marginTop":"10px", "padding":"10px 15px", "cursor":"pointer", "background":"red", "color":"white" ,"border":"none", "borderRadius":"5px"}} onClick={()=> setDisplay(prev => !prev)}>Close</button>
                 </div>
-                <button onClick={upLoadAll}>Upload All</button>
+                {/* <button onClick={upLoadAll}>Upload All</button> */}
             </form>
             }
         </div>
